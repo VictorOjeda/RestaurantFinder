075a4931d0d5a2117b9f71cdf444f074a9d0c941
VictorOjeda <victorm.ojedav@gmail.com>
2020-08-29 18:18:54 -0700

    Setup GraphQL

---
+++ app/src/main/java/com/victorojeda/restaurantfinder/RestaurantFinderActivity.kt
@@ -0,0 +1,12 @@
+package com.victorojeda.restaurantfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestaurantFinderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_finder)
+
+    }
+}
